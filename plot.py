bc61f7c1c7560fd9b36cd4894bda44e362540fce
icvara <icvara.barbier@gmail.com>
2021-07-11 23:28:24 +0100

    small improvmnt in plot

--- plot.py
+++ plot.py
@@ -11,38 +11,33 @@ import statistics
 from collections import Counter
 
 ##par from abc smc
-number="final"
-version=""
+n="final"
 
-<<<<<<< HEAD
-path = 'smc_'+'/pars_' + number + '.out'
-dist_path = 'smc_'+'/distances_' + number + '.out'
-=======
+def load(number= n):
 
-path = 'smc_'+version+'/pars_' + number + '.out'
-dist_path = 'smc_'+version+'/distances_' + number + '.out'
->>>>>>> d02312206050282356c7137257addac1156ffbe8
+    path = 'smc_'+'/pars_' + number + '.out'
+    dist_path = 'smc_'+'/distances_' + number + '.out'
 
-raw_output= np.loadtxt(path)
-dist_output= np.loadtxt(dist_path)
-df = pd.DataFrame(raw_output, columns = ['K_ARAX','n_ARAX','K_XY','n_XY','K_XZ','n_XZ', 'beta_X','alpha_X','delta_X',
-                                          'K_ARAY','n_ARAY','K_YZ','n_YZ', 'beta_Y','alpha_Y','delta_Y',
-                                          'K_ZX','n_ZX', 'beta_Z','alpha_Z','delta_Z'])
-df['dist']=dist_output
+    raw_output= np.loadtxt(path)
+    dist_output= np.loadtxt(dist_path)
+    df = pd.DataFrame(raw_output, columns = ['K_ARAX','n_ARAX','K_XY','n_XY','K_XZ','n_XZ', 'beta_X','alpha_X','delta_X',
+                                              'K_ARAY','n_ARAY','K_YZ','n_YZ', 'beta_Y','alpha_Y','delta_Y',
+                                              'K_ZX','n_ZX', 'beta_Z','alpha_Z','delta_Z'])
+    df['dist']=dist_output
 
 
 
-# print(df[df['dist']==min(df['dist'])])
+    # print(df[df['dist']==min(df['dist'])])
 
-p_min=df[df['dist']==min(df['dist'])]
+    p_min=df[df['dist']==min(df['dist'])]
 
-p=[p_min['K_ARAX'].tolist()[0],p_min['n_ARAX'].tolist()[0],p_min['K_XY'].tolist()[0],p_min['n_XY'].tolist()[0],p_min['K_XZ'].tolist()[0],p_min['n_XZ'].tolist()[0], 
-p_min['beta_X'].tolist()[0],p_min['alpha_X'].tolist()[0],p_min['delta_X'].tolist()[0],
-p_min['K_ARAY'].tolist()[0],p_min['n_ARAY'].tolist()[0],p_min['K_YZ'].tolist()[0],p_min['n_YZ'].tolist()[0], p_min['beta_Y'].tolist()[0],
-p_min['alpha_Y'].tolist()[0],p_min['delta_Y'].tolist()[0],
-p_min['K_ZX'].tolist()[0],p_min['n_ZX'].tolist()[0], p_min['beta_Z'].tolist()[0],p_min['alpha_Z'].tolist()[0],p_min['delta_Z'].tolist()[0]]
+    p=[p_min['K_ARAX'].tolist()[0],p_min['n_ARAX'].tolist()[0],p_min['K_XY'].tolist()[0],p_min['n_XY'].tolist()[0],p_min['K_XZ'].tolist()[0],p_min['n_XZ'].tolist()[0], 
+    p_min['beta_X'].tolist()[0],p_min['alpha_X'].tolist()[0],p_min['delta_X'].tolist()[0],
+    p_min['K_ARAY'].tolist()[0],p_min['n_ARAY'].tolist()[0],p_min['K_YZ'].tolist()[0],p_min['n_YZ'].tolist()[0], p_min['beta_Y'].tolist()[0],
+    p_min['alpha_Y'].tolist()[0],p_min['delta_Y'].tolist()[0],
+    p_min['K_ZX'].tolist()[0],p_min['n_ZX'].tolist()[0], p_min['beta_Z'].tolist()[0],p_min['alpha_Z'].tolist()[0],p_min['delta_Z'].tolist()[0]]
 
-p_final=abc_smc.pars_to_dict(p)
+    p_final=abc_smc.pars_to_dict(p)
 
 ## predefined par
 par = {
@@ -55,7 +50,7 @@ par = {
     'K_XY':0.01,
     'K_YZ':0.01,
     
-    'K_XZ':0.05,#4.05, #1e10
+    'K_XZ':1e10,#0.05,#4.05, #1e10
     
     'n_ZX':2,
     'n_XZ':2,
@@ -105,14 +100,14 @@ def plot(ARA,par,name):
 
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    ax.plot(X[:,0],Y[:,0],Z[:,0],'-')
+    ax.plot(X[:,-1],Y[:,-1],Z[:,-1],'-')
     plt.savefig(name+'_3D'+'.pdf', bbox_inches='tight')
     plt.close()
 
 
-    plt.plot(X[:,0],'-r')
-    plt.plot(Y[:,0],'-b')
-    plt.plot(Z[:,0],'-g')
+    plt.plot(X[:,-1],'-r')
+    plt.plot(Y[:,-1],'-b')
+    plt.plot(Z[:,-1],'-g')
     plt.ylim(0,1)
     plt.plot([20/0.1, 20/0.1], [0, 1], 'k--', lw=1)
     plt.savefig(name+'_time'+'.pdf', bbox_inches='tight')
@@ -120,7 +115,6 @@ def plot(ARA,par,name):
     #plt.show()
 
 
-<<<<<<< HEAD
 
 def par_plot(df):
     sns.pairplot(df[['K_ARAX','n_ARAX','K_XY','n_XY','K_XZ','n_XZ', 'beta_X','alpha_X','delta_X',
@@ -133,22 +127,6 @@ def par_plot(df):
                                               'K_ARAY','n_ARAY','K_YZ','n_YZ',
                                               'K_ZX','n_ZX']], kind='kde')
     plt.savefig('K_par_plot.pdf', bbox_inches='tight')
-=======
-sns.pairplot(df[['K_ARAX','n_ARAX','K_XY','n_XY','K_XZ','n_XZ', 'beta_X','alpha_X','delta_X',
-                                          'K_ARAY','n_ARAY','K_YZ','n_YZ', 'beta_Y','alpha_Y','delta_Y',
-                                          'K_ZX','n_ZX', 'beta_Z','alpha_Z','delta_Z']], kind='kde')
-plt.savefig(version+'_par_plot.pdf', bbox_inches='tight')
-
-plt.close()
-sns.pairplot(df[['K_ARAX','n_ARAX','K_XY','n_XY','K_XZ','n_XZ',
-                                          'K_ARAY','n_ARAY','K_YZ','n_YZ',
-                                          'K_ZX','n_ZX']], kind='kde')
-plt.savefig(version+'_K_par_plot.pdf', bbox_inches='tight')
-
-plt.close()
-sns.pairplot(df[['beta_X','alpha_X','delta_X', 'beta_Y','alpha_Y','delta_Y', 'beta_Z','alpha_Z','delta_Z']], kind='kde')
-plt.savefig(version+'_beta_par_plot.pdf', bbox_inches='tight')
->>>>>>> d02312206050282356c7137257addac1156ffbe8
 
     plt.close()
     sns.pairplot(df[['beta_X','alpha_X','delta_X', 'beta_Y','alpha_Y','delta_Y', 'beta_Z','alpha_Z','delta_Z']], kind='kde')
@@ -157,4 +135,4 @@ plt.savefig(version+'_beta_par_plot.pdf', bbox_inches='tight')
 
 
 
-plot(ARA,p_final,"ABC_SMC")
+plot(ARA,par,"rep")
